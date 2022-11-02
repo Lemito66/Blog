a292a5a1e0e7be884d975fb1cec95ab9d806101f
Emill <lemito66@gmail.com>
2022-11-02 13:59:15 -0500

    Listo para el feriado

--- theblog/views.py
+++ theblog/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 # Create your views here.
 """ def home(request):
     return render(request, 'home.html') """
     
 class HomeView(ListView):
+    '''
+        Crea una vista para visualizar
+    '''
     model = Post
     template_name = 'home.html'
     
 class ArticleDetailView(DetailView):
+    '''
+        Crea una vista para ver los detalles del post
+    '''
     model = Post
     template_name = 'article_details.html'
+    
+class AddPostView(CreateView):
+    '''
+        Crea una vista para crear una vista
+    '''
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__' # Todos los campos
